2c13474f8bddfa73757e61118ac9476f751a2b3a
DELL <hanaaaldaly@gmail.com>
2019-08-25 02:25:14 +0200

    save

---
+++ app/src/main/java/com/hanaa/iti/testcoverage/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hanaa.iti.testcoverage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
